3f77b1b305c174f8ab8589dc8b7c06a1056007ac
xavi <xavier.stoa@gmail.com>
2023-12-08 02:46:56 +0100

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,33 @@
 <template>
     <Header></Header>
     <div class="flex">
-        <div class="flex items-center justify-center h-screen max-w-70 space-x-4">
-            <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h2> D’où vient la désinformation ? </h2>
-                <p class="mt-5">Elle vient principalement d’entreprises tirant leurs bénéfices d’énergies fossiles. Parmi elles,
-                    la CAAD a identifié Shell, BP ou encore Total Energies.</p>
-            </div>
-            <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h2> Qui sont les climatosceptiques ?</h2>
-                <p class="mt-5">Il existe aussi les climatosceptiques, une catégorie de personnes qui ne sont pas en très
-                    grand nombre, mais qui compensent cette infériorité numérique par un flot extrêmement grand de posts sur les
-                    réseaux sociaux. Ces comptes ont généralement été identifiés comme repostant aussi du contenu anti-vaccin,
-                    pro-russes mais aussi d'extrême droite.</p>
-            </div>
-            <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h2>Où retrouve-t-on ces climatosceptiques ?</h2>
-                <p class="mt-5">Les réseaux sociaux identifiés comme le plus sujet à ces groupes climato-sceptiques sont principalement X (anciennement Twitter), youtube ou encore Facebook.
-</p>
-            </div>
+      <div class="flex items-center justify-center h-screen w-full space-x-4">
+        <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h2> D’où vient la désinformation ? </h2>
+          <p class="mt-5">Elle vient principalement d’entreprises tirant leurs bénéfices d’énergies fossiles. Parmi elles,
+            la CAAD a identifié Shell, BP ou encore Total Energies.</p>
         </div>
-        <div>
-            <a href="https://serrata.super-sympa.fr" class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h4>Publicité</h4>
-                <h2>Jouez à serrata!</h2>
-                <img src="assets/img/globe.png" alt="Globe avec drapeaux du monde" class="width-50">
-                <p>Un jeu de culture générale et de rapidité au clavier.</p>
-            </a>
+        <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h2> Qui sont les climatosceptiques ?</h2>
+          <p class="mt-5">Il existe aussi les climatosceptiques, une catégorie de personnes qui ne sont pas en très
+            grand nombre, mais qui compensent cette infériorité numérique par un flot extrêmement grand de posts sur les
+            réseaux sociaux. Ces comptes ont généralement été identifiés comme repostant aussi du contenu anti-vaccin,
+            pro-russes mais aussi d'extrême droite.</p>
         </div>
+        <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h2>Où retrouve-t-on ces climatosceptiques ?</h2>
+          <p class="mt-5">Les réseaux sociaux identifiés comme le plus sujet à ces groupes climato-sceptiques sont principalement X (anciennement Twitter), youtube ou encore Facebook.</p>
+        </div>
+      </div>
+      <div class="flex items-start justify-end w-full">
+        <a href="https://serrata.super-sympa.fr" class="max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h4>Publicité</h4>
+          <img src="assets/img/globe.png" alt="Globe avec drapeaux du monde" class="w-50">
+          <h2>Jouez à serrata!</h2>
+          <p>Un jeu de culture générale et de rapidité au clavier.</p>
+        </a>
+      </div>
     </div>
     <Footer></Footer>
-</template>
+  </template>
+  
